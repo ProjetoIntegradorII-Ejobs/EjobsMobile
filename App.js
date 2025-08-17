4d3c7bbdac8441de9b1fa408bc218fc7a0092f0f
Victorgbatista <47211446+Victorgbatista@users.noreply.github.com>
2025-08-17 18:03:28 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,20 +3,27 @@ import { StatusBar } from 'expo-status-bar';
 import { View, Text, Button, Alert} from "react-native";
 import {css} from './assets/css/Css';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './views/Home';
 import Login from './views/Login';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{ title: 'Página Inicial' }}
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ title: 'Acesso' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
