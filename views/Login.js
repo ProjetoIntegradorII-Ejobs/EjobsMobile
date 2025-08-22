fdfa99677de9cff28983e3c9037e2f26e652e42c
Victorgbatista <47211446+Victorgbatista@users.noreply.github.com>
2025-08-21 21:50:42 -0300

    Teste com NodeJs como Backend

--- views/Login.js
+++ views/Login.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Text, View} from 'react-native';
+import { Text, View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity} from 'react-native';
+import {css} from '../assets/css/Css';
+
+// A Mensagem será usada dinamicamente no futuro, eu espero 
 
 export default function Login({route})
 {
     return(
-      <View>
-          <Text>Esse é o componente Login do sistema {route.params.id}</Text>
-      </View>
+      <KeyboardAvoidingView 
+        style={[css.container, css.bg]}>
+        
+        
+        <View>
+          <Text style={css.login__msg }> Usuário ou senha Inválido</Text>
+        </View>
+
+        <View style={css.login__form}>  
+            
+            <TextInput style={css.login__input} placeholder='Informe seu Email' />
+            <TextInput style={css.login__input} placeholder='Informe sua senha' secureTextEntry={true} />
+            <TouchableOpacity style={css.login__button}>
+              <Text style={css.login__buttonText}>Entrar </Text>
+            </TouchableOpacity>
+
+        </View>
+      </KeyboardAvoidingView>
     );
 }
